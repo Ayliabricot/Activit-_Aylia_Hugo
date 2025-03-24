d4f6b60f5234430c2ed29c74eb48d033999098c9
Aylia <aylia.sabadie@student.junia.com>
2025-03-24 14:17:53 +0100

    Creer utilisateur

--- ReseauSocial/fonctions.c
+++ ReseauSocial/fonctions.c
@@ -2,3 +2,24 @@
 #include <stdlib.h>
 #include "header.h"
 
+Utilisateur** creer_liste_utilisateurs(void) {
+	Utilisateur** liste = malloc(sizeof(Utilisateur*));
+	*liste = NULL;
+
+	return liste;
+}
+
+Utilisateur* creer_utilisateur(int id) {
+	char pseudo[20];
+
+	printf("\nVeuillez saisir votre pseudo : ");
+	scanf_s("%s", &pseudo, sizeof(pseudo));
+
+	Utilisateur* utilisateur = malloc(sizeof(Utilisateur));
+	utilisateur->id = id;
+	strcpy_s(utilisateur->pseudo, 20, pseudo);
+	utilisateur->premier_ami = NULL;
+	utilisateur->premier_post = NULL;
+
+	return utilisateur;
+}
--- ReseauSocial/header.h
+++ ReseauSocial/header.h
@@ -2,14 +2,17 @@
 #define RESEAU
 
 typedef struct {
-	int identifiant;
+	int id;
 	char pseudo[20];
-	Utilisateur premier_ami;
-	Publication premier_post;
+	Utilisateur* premier_ami;
+	Publication* premier_post;
 }Utilisateur;
 
 typedef struct {
 	char texte[1000];
 }Publication;
+
+Utilisateur** creer_liste_utilisateurs(void);
+Utilisateur* creer_utilisateur(int id);
 
 #endif
